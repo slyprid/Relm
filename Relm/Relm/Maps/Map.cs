605e4785880248d59c298d6fb998ce948128234b
Aaron Foley <afoley@linq.com>
2021-07-16 14:18:11 -0400

    Added ID's to Maps
    Added ID's to Maps

--- Relm/Relm/Maps/Map.cs
+++ Relm/Relm/Maps/Map.cs
@@ -1,7 +1,15 @@
-﻿namespace Relm.Maps
+﻿using System;
+
+namespace Relm.Maps
 {
     public class Map
     {
+        public Guid Id { get; set; }
         public string Name { get; set; }
+
+        public Map()
+        {
+            Id = Guid.NewGuid();
+        }
     }
 }
