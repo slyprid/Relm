dce0b5b774533ccb64b2f42f73e2e215e731977c
Aaron Foley <afoley@linq.com>
2021-07-18 09:35:21 -0400

    Added color extension so can tween colors
    Added color extension so can tween colors

---
+++ Relm/Relm/Extensions/ColorEx.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace Relm.Extensions
+{
+    public class ColorEx
+    {
+        public Color Color { get; private set; }
+
+        public Vector3 Value3
+        {
+            get => Color.ToVector3();
+            set => Color = new Color(value);
+        }
+
+        public Vector4 Value4
+        {
+            get => Color.ToVector4();
+            set => Color = new Color(value);
+        }
+        
+
+        public ColorEx()
+        {
+            Color = Color.White;
+        }
+
+        public ColorEx(Color color)
+        {
+            Color = color;
+        }
+    }
+}
