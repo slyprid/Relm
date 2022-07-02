c1f9cd7565e874d31947e5be7e09ef6fc59f59a5
Aaron Foley <aaronfoley80@gmail.com>
2022-07-02 07:35:40 -0400

    Work on save files and serialization
    Work on save files and serialization

---
+++ Relm/Relm/Extensions/SerializationExtensions.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace Relm.Extensions
+{
+    public static class SerializationExtensions
+    {
+        public static T DeserializeXml<T>(this string input)
+        {
+            var xmlSerialize = new XmlSerializer(typeof(T));
+            using (var reader = new StringReader(input))
+            {
+                return (T)xmlSerialize.Deserialize(reader);
+            }
+        }
+
+        public static string SerializeXml<T>(this T input)
+        {
+            var xmlSerializer = new XmlSerializer(typeof(T));
+            using (var writer = new StringWriter())
+            {
+                xmlSerializer.Serialize(writer, input);
+                return writer.ToString();
+            }
+        }
+
+        public static T DeserializeJson<T>(this string input)
+        {
+            return JsonConvert.DeserializeObject<T>(input);
+        }
+
+        public static string SerializeJson<T>(this T input)
+        {
+            return JsonConvert.SerializeObject(input);
+        }
+    }
+}
---
+++ Relm/Relm/Output/SaveFile.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+using Newtonsoft.Json;
+using Relm.Extensions;
+
+namespace Relm.Output
+{
+    [Serializable]
+    public class SaveFile<T>
+        where T : class
+    {
+        [XmlIgnore]
+        [JsonIgnore]
+        public virtual string OutputDirectory { get; set; }
+
+        [XmlIgnore]
+        [JsonIgnore]
+        public virtual string Filename { get; set; }
+
+        public virtual void Save()
+        {
+            var xml = this.SerializeXml();
+            var path = Path.Combine(OutputDirectory, Filename);
+
+            if (!Directory.Exists(OutputDirectory))
+            {
+                Directory.CreateDirectory(OutputDirectory);
+            }
+
+            File.WriteAllText(path, xml);
+        }
+
+        public virtual T Load()
+        {
+            var path = Path.Combine(OutputDirectory, Filename);
+            var xml = File.ReadAllText(path);
+            return xml.DeserializeXml<T>();
+        }
+    }
+}
