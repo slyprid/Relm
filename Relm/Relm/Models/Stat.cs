702b9caab365ac67c3d4cf7d425e4c665e9e88da
Aaron Foley <aaronfoley80@gmail.com>
2022-07-09 18:09:55 -0400

    Added Stat class
    Added Stat class

---
+++ Relm/Relm/Models/Stat.cs
@@ -0,0 +1,28 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Relm.Models
+{
+    public class Stat<T>
+    {
+        public T Minimum { get; set; }
+        public T Maximum { get; set; }
+        public T Value { get; set; }
+        public T AdjustedMaximum { get; set; }
+
+        public Stat() { }
+        
+        public Stat(T minimum, T maximum, T value)
+        {
+            Minimum = minimum;
+            Maximum = maximum;
+            Value = value;
+        }
+
+        public Stat(T minimum, T maximum)
+        {
+            Minimum = minimum;
+            Maximum = maximum;
+            Value = Maximum;
+        }
+    }
+}
